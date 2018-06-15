34ef4117892116a7d039b07c898a7e8bfa260587
aikawa1993 <aikawa_kajuhiko34@yahoo.com>
2018-06-15 17:43:27 +0700

    ~ commit

---
+++ Banhang/Banhang/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Banhang.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutPage()
+        {
+            return View();
+        }
+	}
+}

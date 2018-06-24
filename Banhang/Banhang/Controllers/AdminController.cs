365b90f6185898ee1c5407769fbecda3d0e01609
aikawa1993 <36188885+aikawa1993@users.noreply.github.com>
2018-06-24 19:31:19 +0700

    ~ commit

--- Banhang/Banhang/Controllers/AdminController.cs
+++ Banhang/Banhang/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Banhang.Controllers
 {
@@ -12,7 +13,55 @@ namespace Banhang.Controllers
         // GET: /Admin/
         public ActionResult Index()
         {
+            ViewBag.User = this.Session["User"];
             return View();
         }
-	}
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            //    using (LittlePrinceBakeryDataContext db = new LittlePrinceBakeryDataContext())
+            //    {
+            //        var v = db.Staffs.Where(a => a.StaffID.Equals(username) && a.Password.Equals(pass)).FirstOrDefault();
+            //        if (v != null)
+            //        {
+            //            this.Session["ID"] = v.StaffID;
+            //            this.Session["StaffName"] = v.StaffName;
+            //            this.Session["Password"] = v.Password;
+            //            this.Session["Role"] = v.RoleID;
+            //            this.Session["State"] = v.State;
+            //            if (v.RoleID.Equals("AD"))
+            //                return RedirectToAction("Admin");
+            //            else if (v.RoleID.Equals("MS") && v.State.Equals(true))
+            //                return RedirectToAction("Index", "ManageOrder");
+            //            else if (v.RoleID.Equals("SH") && v.State.Equals(true))
+            //                return RedirectToAction("ShIndex", "ManageOrder");
+            //            //if (v.RoleID.Equals("AD"))
+            //            //    return RedirectToAction("Admin");
+            //            //else if (v.RoleID.Equals("MS"))
+            //            //    return RedirectToAction("Index", "ManageOrder");
+            //            //else if (v.RoleID.Equals("SH"))
+            //            //    return RedirectToAction("ShIndex", "ManageOrder");
+            //        }
+            //    }
+            //}
+            if (username == "admin" && password == "123456")
+            {
+                this.Session["User"] = username;
+                return RedirectToAction("Index", "Admin");
+            }
+            return RedirectToAction("Error", "Admin");
+        }
+      
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("Login");
+        }
+    }
 }
